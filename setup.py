e1db0dfe6709ebc2f3687d0ac92de0163006fee4
Ben Snow <56250881+BenSnow6@users.noreply.github.com>
2022-06-28 12:39:09 +0100

    Initial cookie cutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Productionising a depth estimation machine learning project',
+    author='BenSnow6',
+    license='MIT',
+)
